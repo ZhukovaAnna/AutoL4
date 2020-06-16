e2b560cbe37d99063bd0808e8ef066c6eaeaee86
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-06-16 15:57:46 +0300

    Add test

---
+++ src/test/java/ru/netology/rest/TestForm.java
@@ -0,0 +1,36 @@
+package ru.netology.rest;
+
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.cssSelector;
+
+public class TestForm {
+    private LocalDate today = LocalDate.now();
+    private LocalDate date = today.plusDays(5);
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys("Москва");
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(date.format(formatter));
+        form.$(cssSelector("[name=name]")).sendKeys("Иванова Анна");
+        form.$(cssSelector("[name=phone]")).sendKeys("+79109876543");
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Забронировать")).click();
+        $(cssSelector(".notification__title")).waitUntil(Condition.visible, 15000);
+    }
+}
+    
